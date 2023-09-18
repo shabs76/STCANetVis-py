0a408820bc09b74b5bd13a75ec0e78fa39948c61
shabs76 <shabsmwanga76@gmail.com>
2023-09-19 00:47:51 +0300

    changing number of workers gunicorn

--- Modal_Data/gunicorn_config.py
+++ Modal_Data/gunicorn_config.py
@@ -1,5 +1,5 @@
 # gunicorn_config.py
 
-workers = 4  # Number of worker processes
+workers = 1  # Number of worker processes
 bind = "0.0.0.0:8000"  # IP address and port to bind to
 timeout = 120  # Maximum time a request is allowed to process
